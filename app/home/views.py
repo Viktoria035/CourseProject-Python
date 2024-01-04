161711744f23ede7f0a3f98ef129c48ee68509a9
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-01-04 23:32:59 +0200

    get_quiz function in views(app)

--- app/home/views.py
+++ app/home/views.py
@@ -1,6 +1,26 @@
-from django.http import HttpResponse
-
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from .models import *
+import random
 
 def response(request):
     return HttpResponse("Hello from Django!")
+
+def get_quiz(request):
+    try:
+        question_objs = Question.objects.all()
+        data = []
+        for question_obj in question_objs:
+            data.append({
+                "category":question_obj.category.categoty_name,
+                "question":question_obj.question,
+                "answer":question_obj.marks
+            })
+        
+        payload = {'status':True, 'data':data}
+
+        return JsonResponse(payload)
+    except Exception as e:
+        print(e)
+        return HttpResponse("Something went wrong!")
+    
