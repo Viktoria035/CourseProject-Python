c5e0b912e50ed250ff952925fd1d8dad92890c54
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-01-04 22:47:14 +0200

    home added to installed_apps

---
+++ app/home/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+import uuid
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Category(BaseModel):
+    categoty_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.categoty_name
+
+
+class Question(BaseModel):
+    category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE)
+    question = models.CharField(max_length=500)
+    marks = models.IntegerField(default=1)
+
+    def __str__(self) -> str:
+        return self.question
+
+
+class Answer(BaseModel):
+    question = models.ForeignKey(Question, related_name='question_answer', on_delete=models.CASCADE)
+    answer = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
+    #points = models.IntegerField(default=10)
+
+    def __str__(self) -> str:
+        return self.answer
