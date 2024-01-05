4936fdcfb75e336331666bcc33bdecda870e6ca8
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-01-05 09:38:04 +0200

    Include logout URL

--- app/gui/urls.py
+++ app/gui/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login', auth_views.LoginView.as_view())
+    path('', views.index),
+    path('login', auth_views.LoginView.as_view()),
+    path('logout', auth_views.LogoutView.as_view())
 ]
