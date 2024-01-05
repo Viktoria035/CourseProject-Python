bbec91bcce8f6927eb29f2377f849e415f05d064
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-01-05 09:21:26 +0200

    polls deleted and Expand project URLs with the GUI APP URLs

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("gui.urls"))
     path("home/", include("home.urls")),
-    path("polls/", include("polls.urls")),# should delete
     path('admin/', admin.site.urls),
 ]
