3f7c2bf6c5c61aa9382480b358dec72a90011cd1
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-01-05 23:28:11 +0200

    Improve UX

--- app/gui/forms.py
+++ app/gui/forms.py
@@ -3,7 +3,13 @@ from django.contrib.auth.models import User
 
 class RegisterUserForm(UserCreationForm):
 
-	class Meta:
-		model = User
-		fields = ("username", "email",
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
+    class Meta:
+        model = User
+        fields = ("username", "email",
                   "password1", "password2")
