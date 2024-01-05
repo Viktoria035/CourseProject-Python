12f1b2c071b3a9915b84a585989c32024b3fc7d7
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-01-05 23:15:25 +0200

    Allow registration of new users

--- app/gui/views.py
+++ app/gui/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth import login
+from .forms import RegisterUserForm
 
 @login_required(login_url='/login')
 def index(request):
     """Welcome page."""
-    return render(request, 'index.html')
+
+    context = {
+        'username': request.user.username
+    }
+    return render(request, 'index.html', context)
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(index)
+    else:
+        form = RegisterUserForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/register.html', context)
