288e17ba1496db9aaae262b42fa4a529c9b4d942
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-01-12 22:24:24 +0200

    predefine str function

--- app/gui/models.py
+++ app/gui/models.py
@@ -7,3 +7,6 @@ class User(models.Model):
     score=models.IntegerField(default=0)
     position=models.IntegerField(default=0)
     title=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
