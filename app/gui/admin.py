c7ea353c413a75d476cdf366c7211c4fe7d423bd
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-01-12 22:40:11 +0200

    profile model’s fields to the user page in the admin

--- app/gui/admin.py
+++ app/gui/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from gui.models import UserQ
+
+
+class UserQInLine(admin.StackedInline):
+    model = UserQ
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [UserQInLine]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- app/gui/models.py
+++ app/gui/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
+class UserQ(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
     score=models.IntegerField(default=0)
     position=models.IntegerField(default=0)
