1bc041ff36923280b34724e51df5c400377f53ee
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-01-12 22:20:50 +0200

    class User in models(gui) created

--- app/gui/models.py
+++ app/gui/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    score=models.IntegerField(default=0)
+    position=models.IntegerField(default=0)
+    title=models.CharField(max_length=200)
