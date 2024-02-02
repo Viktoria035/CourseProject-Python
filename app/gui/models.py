30aa81ac36a7b721764ead737ac418b949830721
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-02-02 08:49:02 +0200

    question and choice classes created

--- app/gui/models.py
+++ app/gui/models.py
@@ -10,3 +10,22 @@ class Player(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
+class Question(models.Model):
+    question = models.CharField(max_length=500)
+    max_marks=models.DecimalField(default=0,decimal_places=2,max_digits=6)
+    category = models.CharField(max_length=200)
+    #answer = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=500)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice
