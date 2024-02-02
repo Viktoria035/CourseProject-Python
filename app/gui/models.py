de3db190525d6548b11666a74bf6909d692367ff
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-02-02 10:06:50 +0200

    quizz class changed

--- app/gui/models.py
+++ app/gui/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.postgres import fields
 from django.utils.translation import ugettext as _
 from django.core.validators import MaxValueValidator
 import re
@@ -130,44 +129,44 @@ class Quizz(models.Model):
         return str(self.id) + "_data"
 
 
-class Question(models.Model):
-    question = models.CharField(max_length=500)
-    max_marks=models.DecimalField(default=0,decimal_places=2,max_digits=6)
-    category = models.CharField(max_length=200)
-    #answer = models.CharField(max_length=200)
+# class Question(models.Model):
+#     question = models.CharField(max_length=500)
+#     max_marks=models.DecimalField(default=0,decimal_places=2,max_digits=6)
+#     category = models.CharField(max_length=200)
+#     #answer = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.question
-
-
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    correct_answer = models.CharField(max_length=500)
-    #is_correct = models.BooleanField(default=False)
+#     def __str__(self):
+#         return self.question
 
 
-    class Meta:
-        abstract = True
+# class Answer(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     correct_answer = models.CharField(max_length=500)
+#     #is_correct = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.correct_answer
+
+#     class Meta:
+#         abstract = True
+
+#     def __str__(self):
+#         return self.correct_answer
     
-    def is_correct(self, user_answer):
-        return self.correct_answer == user_answer
+#     def is_correct(self, user_answer):
+#         return self.correct_answer == user_answer
     
-#check
-class FreeTextAnswer(Answer):
-    case_sensitive = models.BooleanField(default=False)
+# #check
+# class FreeTextAnswer(Answer):
+#     case_sensitive = models.BooleanField(default=False)
 
-    def is_correct(self, user_answer):
-        if self.case_sensitive:
-            return self.correct_answer == user_answer
-        else:
-            return self.correct_answer.lower() == user_answer.lower()
+#     def is_correct(self, user_answer):
+#         if self.case_sensitive:
+#             return self.correct_answer == user_answer
+#         else:
+#             return self.correct_answer.lower() == user_answer.lower()
         
-#check
-class MultipleChoiceAnswer(Answer):
-    choices = fields.ArrayField(models.CharField(max_length=200, blank=True))
+# #check
+# class MultipleChoiceAnswer(Answer):
+#     choices = models.ManyToManyField(Answer, blank=True)
 
-    def __str__(self) -> str:
-        return f"{self.correct_answer} from {self.choices}"
+#     def __str__(self) -> str:
+#         return f"{self.correct_answer} from {self.choices}"
