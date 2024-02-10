fc41dc39d73bbf3956d337b0ccc27c18024b9925
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-02-10 09:46:09 +0200

    View quiz list by category

--- app/gui/views.py
+++ app/gui/views.py
@@ -1,9 +1,14 @@
+from typing import Any
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from .forms import RegisterUserForm
 from app.functions import calculate_leaderboard_rank
-from .models import Player
+from .models import Player, Quizz, Category
+from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
 # @login_required(login_url='/login')
 def index(request):
     """Welcome page."""
@@ -49,10 +54,28 @@ def question(request):
 def leaderboard(request):
     """Leaderboard page."""
     players = Player.objects.all().order_by('-score')
-    
+
     context = {
         'players': players,
         'auth': request.user.is_authenticated
     }
     return render(request, 'question/leaderboard.html', context=context)
 
+
+class ViewQuizListByCategory(ListView):
+    model = Quizz
+    template_name = 'question/quiz_category.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.category = get_object_or_404(Category, category=self.kwargs['category'])
+
+        return super(ViewQuizListByCategory, self).dispatch(request, *args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super(ViewQuizListByCategory, self).get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
+    
+    def get_queryset(self):
+        queryset = self.category.quizz_set.all()
+        return queryset
